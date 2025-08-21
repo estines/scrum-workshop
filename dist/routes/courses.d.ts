40a35637f3794bbf7b4edbcdb8d7403c584f28dd
noppornw <noppornw@tisco.co.th>
2025-08-22 01:58:16 +0700

    ignore build folder

--- dist/routes/courses.d.ts
+++
@@ -1,2 +0,0 @@
-import { GoogleSheetsService } from '../services/googleSheets';
-export declare function createCoursesRouter(sheetsService: GoogleSheetsService): import("express-serve-static-core").Router;
